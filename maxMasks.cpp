d90eb5fdccc90bbed320ec9f5148f3f003de45f5
hoangi19 <viethoanghy99@gmail.com>
2019-08-28 14:10:56 +0700

    update to test.txt

--- maxMasks.cpp
+++ maxMasks.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <queue>
 #include <string>
+#include "checkMasks.cpp"
 
 int loadName(std::vector<std::string> &listName, std::string path){
     DIR *dr;
@@ -35,15 +36,16 @@ bool comp(std::pair<int, int> p, std::pair<int, int> q){
     return p.first > q.first;
 }
 
-int getSMasks(std::vector<std::string> listName, std::string path, std::pair<int, int> SMasks[]){
+int getSMasks(std::vector<std::string> listName, std::vector<cv::Mat> listImages, std::string path, std::pair<int, int> SMasks[]){
     for (int i = 0; i < listName.size(); i++){
-        cv::Mat image = cv::imread(path + listName[i], 0);
+        cv::Mat image = listImages[i];
         // cv::imshow(listName[i], image);
         // cv::waitKey(0);
         for (int r = 0; r < image.rows; ++r)
             for (int c = 0; c < image.cols; ++c)
                 if ((int)image.at<uchar>(r, c) != 0) SMasks[i].first++;
     }
+    // std::cout << "hi\n";
     std::sort(SMasks, SMasks + listName.size(), comp);
     return 1;
 }
@@ -66,7 +68,7 @@ int writeImage(std::string name, std::string outP, std::string path){
 }
 
 int loadMasks(std::string inpath, std::string outpath){
-    std::string arr[] = {"bladder", "prostate", "left femur", "rectum", "right femur"};
+    std::string arr[] = {"left femur", "bladder", "prostate" , "rectum", "right femur"};
     DIR *dip;
     if ( (dip = opendir(inpath.c_str())) == NULL) return printf("Folder %s not found!", inpath), 1;
     struct dirent *de;
@@ -79,81 +81,101 @@ int loadMasks(std::string inpath, std::string outpath){
         std::string rpath = inpath + "/" + de->d_name + "/";
         for (int i = 0; i < 5; ++i)
         {
-        std::string path = rpath + arr[i] + "/predict_pos/";
+            // for (int idx = 0; idx < arr[i].size(); i++)
+            //     if (arr[i][idx] == ' ') arr[i][idx] = ;
+            std::string path =  rpath  + arr[i]   + "/predict/";
 
-        if (loadName(listName, path)) continue;
-        // create folder if not exist
-        std::string outP = outpath + "/" + de->d_name + "/" + arr[i] + "/predict_pos/";
-        if (listName.size() == 0){
-            std::cout << path << " is empty!";
-        }
-        std::pair<int, int> SMasks[listName.size()];
-        for (int i = 0; i < listName.size(); ++i){
-            SMasks[i] = std::make_pair(0, i);
-        }
-        getSMasks(listName, path, SMasks);
-        float k = 0.2;
-        float sMin = SMasks[0].first * k;
-        std::cout << "Max Size : " << SMasks[0].first << "\n";
-        std::vector<std::string> outName;
-        // writeImage(listName[SMasks[0].second], outP, path);
-        outName.push_back(listName[SMasks[0].second]);
-        for (int i = 1; i < listName.size(); i++){
-            if (SMasks[i].first >= sMin)  outName.push_back(listName[SMasks[i].second]);
-                else std::cout << "del : " << listName[SMasks[i].second] << " size : " << SMasks[i].first << "\n";
-        }
-        
-        std::pair<int, int> outNameInt[outName.size()];
-        for (int i = 0; i < outName.size(); ++i){
-            outNameInt[i] = std::make_pair(0,i);
-        }
-        for (int i = 0; i < outName.size(); ++i){
-            std::string name = outName[i];
-            name.erase(name.size() - 4, 4);
-            // std::cout << name << "x\n";
-            for (int j = 0; j < name.size(); j++)
-                outNameInt[i].first = outNameInt[i].first*10 + (name[j] - '0');
-            // std::cout << outNameInt[i] << "\n";
-        }
-        std::pair<int, int> idmax = outNameInt[0];
-        std::sort(outNameInt, outNameInt + outName.size());
-        int l = 0, r = outName.size()+1;
-        while (r-l > 1)
-        {
-            int m = (r+l)/2;
-            if (outNameInt[m].first == idmax.first){
-                l = m;
-                break;
+            // if (loadName(listName, path)) continue;
+            // create folder if not exist
+            std::vector<cv::Mat> listImages;
+            listImages.clear();
+            listName.clear();
+            
+                // DIR *drxxxx;
+                // std::cout << path << "\n";
+                // drxxxx = opendir(path.c_str());
+                // std::cout << path << "\n";
+                // if (drxxxx == NULL) {std::cout << "error : " << path << "\n"; continue;}
+                // closedir(drxxxx);
+                // delete(drxxxx);
+                // return 1;
+
+            std::string outP = outpath + "/" + de->d_name + "/" + arr[i] + "/predict/";
+            
+            if (checkMasks::loadMasks(path, outP, listImages, listName) == 0) continue;
+            if (listName.size() == 0){
+                std::cout << path << " is empty!";
             }
-            if (outNameInt[m].first > idmax.first) r = m;
-                else r = m;
-        }
-        int ansl = l, ansr = r;
-        r = l + 1;
-        --l;
-        // writeImage(outName[idmax.second], outP, path);
-        // std::cout << "size : " << outName.size() << "\n";
-        
-        while (true)
-        {
-            // std::cout << "l : " << l << " " << " r : " << r << "\n";
-            if (l >= 0) 
-                if (outNameInt[l].first + 5 > outNameInt[l+1].first)  ansl = l; // writeImage(outName[outNameInt[l].second], outP, path);
-                    else  l = -1;
-            if (r < outName.size())
-                if (outNameInt[r].first - 5 < outNameInt[r-1].first) ansr = r; // writeImage(outName[outNameInt[r].second], outP, path);
-                    else r = outName.size();
-            if (l < 0 && r >= outName.size()) break;
-            --l; ++r;
-        }
-        std::cout << ansl << " " << ansr << "\n";
-        std::ofstream outfile;
-        outfile.open("test.txt", std::ios_base::app);
-        
-        outfile << de->d_name << " " << arr[i] << " " << outNameInt[ansl].second << " " << outNameInt[ansr].second << "\n";
-        outfile.close();
-        // delete(&outNameInt);
-        // delete(SMasks);
+            
+            std::pair<int, int> SMasks[listName.size()];
+            for (int i = 0; i < listName.size(); ++i){
+                SMasks[i] = std::make_pair(0, i);
+            }
+            getSMasks(listName, listImages, path, SMasks);
+            // std::cout << listName[1] << "\n";
+            float k = 0.2;
+            float sMin = SMasks[0].first * k;
+            std::cout << "Max Size : " << SMasks[0].first << "\n";
+            std::vector<std::string> outName;
+            // writeImage(listName[SMasks[0].second], outP, path);
+            outName.push_back(listName[SMasks[0].second]);
+            for (int i = 1; i < listName.size(); i++){
+                if (SMasks[i].first >= sMin)  outName.push_back(listName[SMasks[i].second]);
+                    else std::cout << "del : " << listName[SMasks[i].second] << " size : " << SMasks[i].first << "\n";
+            }
+            
+            std::pair<int, int> outNameInt[outName.size()];
+            for (int i = 0; i < outName.size(); ++i){
+                outNameInt[i] = std::make_pair(0,i);
+            }
+            for (int i = 0; i < outName.size(); ++i){
+                std::string name = outName[i];
+                name.erase(name.size() - 4, 4);
+                // std::cout << name << "x\n";
+                for (int j = 0; j < name.size(); j++)
+                    outNameInt[i].first = outNameInt[i].first*10 + (name[j] - '0');
+                // std::cout << outNameInt[i] << "\n";
+            }
+            std::pair<int, int> idmax = outNameInt[0];
+            std::sort(outNameInt, outNameInt + outName.size());
+            int l = 0, r = outName.size()+1;
+            while (r-l > 1)
+            {
+                int m = (r+l)/2;
+                if (outNameInt[m].first == idmax.first){
+                    l = m;
+                    break;
+                }
+                if (outNameInt[m].first > idmax.first) r = m;
+                    else r = m;
+            }
+            int ansl = l, ansr = r;
+            r = l + 1;
+            --l;
+            // writeImage(outName[idmax.second], outP, path);
+            // std::cout << "size : " << outName.size() << "\n";
+            
+            while (true)
+            {
+                // std::cout << "l : " << l << " " << " r : " << r << "\n";
+                if (l >= 0) 
+                    if (outNameInt[l].first + 5 > outNameInt[l+1].first)  ansl = l; // writeImage(outName[outNameInt[l].second], outP, path);
+                        else  l = -1;
+                if (r < outName.size())
+                    if (outNameInt[r].first - 5 < outNameInt[r-1].first) ansr = r; // writeImage(outName[outNameInt[r].second], outP, path);
+                        else r = outName.size();
+                if (l < 0 && r >= outName.size()) break;
+                --l; ++r;
+            }
+            std::cout << ansl << " " << ansr << "\n";
+            std::ofstream outfile;
+            outfile.open("../test.txt", std::ios_base::app);
+            
+            outfile << de->d_name << " " << arr[i] << " " << outNameInt[ansl].first << " " << outNameInt[ansr].first << "\n";
+            std::cout << de->d_name << " " << arr[i] << " " << outNameInt[ansl].first << " " << outNameInt[ansr].first << "\n";
+            outfile.close();
+            // delete(&outNameInt);
+            // delete(SMasks);
         }
     }
     delete(de);
@@ -166,6 +188,11 @@ int main(int argc, char const *argv[])
 {
     std::string input = argv[1];
     std::string output = argv[2];
+    std::ofstream outfile;
+        outfile.open("../test.txt", std::ios::out | std::ios::trunc);
+        // outfile.clear();
+    outfile.close();
     loadMasks(input, output);
+    
     return 0;
 }
